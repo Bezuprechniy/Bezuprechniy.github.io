25af17e2a89dfede70fda102cd4ef3401229dae4
Hagubman <p.beetle11@gmail.com>
2016-08-26 13:40:14 +0300

    adaptive project

---
+++ html/adaptive/pingbuller/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    var count = -100;
+   $('span.left-arrow').click(function() {
+       /*if(count ==  -100) {
+           $('#firstSlide').css('margin-left', count + '%');
+           count -= 100;
+       }*/
+    
+    
+    
+});
